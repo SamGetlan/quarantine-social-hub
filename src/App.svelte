5b4e1fb204ccd6da22a7590830eb14b557f96b52
Sam Getlan <samgetlan@gmail.com>
2020-04-08 03:14:41 -0600

    Integrate top bar with nav side bar

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,27 @@
 	// TODO: disable ts(1192) warning in workspace
 	// @ts-ignore
 	import TopAppBar from './components/TopAppBar.svelte';
+	// @ts-ignore
+	import NavSideBar from './components/NavSideBar.svelte';
+
+	let showSideBar = false;
+
+	const toggleNav = () => {
+		showSideBar = !showSideBar;
+	}
+
+	
 </script>
 
-<TopAppBar />
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<!-- TODO: Refactor this into a layout component -->
+<TopAppBar on:toggleNav={toggleNav}/>
+<NavSideBar bind:show={showSideBar}/>
+<div class="mdc-drawer-app-content mdc-top-app-bar--fixed-adjust">
+	<main>
+		<h1>Hello {name}!</h1>
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	</main>
+</div>
 
 <style lang="scss">
 	$main-color: #ff3e00;
--- src/components/TopAppBar.svelte
+++ src/components/TopAppBar.svelte
@@ -1,16 +1,23 @@
 <script lang="typescript">
-export let title = 'Title';
-import { onMount } from 'svelte';
+import { createEventDispatcher, onMount } from 'svelte';
 import { MDCTopAppBar } from '@material/top-app-bar';
 
+export let title = 'Title';
+
+const dispatch = createEventDispatcher();
+let topAppBar: MDCTopAppBar;
+let topAppBarElement: HTMLBaseElement;
+
 onMount(() => {
-  const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-  const topAppBar = new MDCTopAppBar(topAppBarElement);
-})
+  topAppBar = new MDCTopAppBar(topAppBarElement);
+  topAppBar.listen('MDCTopAppBar:nav', () => {
+      dispatch('toggleNav');
+		});
+});
 
 </script>
   
-<header class="mdc-top-app-bar">
+<header bind:this={topAppBarElement} class="mdc-top-app-bar app-bar">
   <div class="mdc-top-app-bar__row">
     <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
       <button class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button">menu</button>
@@ -22,4 +29,8 @@ onMount(() => {
 <style lang="scss">
   @import "@material/top-app-bar/mdc-top-app-bar";
   @import "@material/icon-button/mdc-icon-button";
+
+  .mdc-top-app-bar {
+	  z-index: 7;
+  }
 </style>
